d82efc650c1d70e96fce4714a1fa0837c6935c92
likeyiyy <929812468@qq.com>
2018-08-23 01:21:00 +0800

    :new: [#] mirrors web
    Signed-off-by: likeyiyy <929812468@qq.com>

--- mirrors/models.py
+++ mirrors/models.py
@@ -1,3 +1,38 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# code is far away from bugs with the god animal protect
+# I love animals. They taste delicious.
+#         ┌─┐       ┌─┐
+#      ┌──┘ ┴───────┘ ┴──┐
+#      │                 │
+#      │       ───       │
+#      │  ─┬┘       └┬─  │
+#      │                 │
+#      │       ─┴─       │
+#      │                 │
+#      └───┐         ┌───┘
+#          │         │
+#          │         │
+#          │         │
+#          │         └──────────────┐
+#          │                        │
+#          │      Gods Bless        ├─┐
+#          │      Never Bugs        ┌─┘
+#          │                        │
+#          └─┐  ┐  ┌───────┬──┐  ┌──┘
+#            │ ─┤ ─┤       │ ─┤ ─┤
+#            └──┴──┘       └──┴──┘
+
 from django.db import models
 
 # Create your models here.
+
+class BaseUser(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    mobile = models.CharField(max_length=64)
+    uid = models.CharField(max_length=200)
+    
+
+class User(models.Model):
+    baseuser =models.ForeignKey(BaseUser, on_delete=models.CASCADE)
--- test_project/settings.py
+++ test_project/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'mirrors.apps.MirrorsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -80,7 +81,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mirrors',
-        'USER': 'mirrors',
+        'USER': 'postgres',
         'PASSWORD': 'mirrors123',
         'HOST': '127.0.0.1',
         'PORT': '5432',
