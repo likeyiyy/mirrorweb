b94af2833712b7543afcfea2f1a556cd5cb130b7
likeyiyy <929812468@qq.com>
2018-08-23 02:17:38 +0800

    :new: [#] mirrors web
    Signed-off-by: likeyiyy <929812468@qq.com>

--- mirrors/models.py
+++ mirrors/models.py
@@ -33,6 +33,34 @@ class BaseUser(models.Model):
     mobile = models.CharField(max_length=64)
     uid = models.CharField(max_length=200)
     
+    def __str__(self):
+        return self.name
+    
 
 class User(models.Model):
-    baseuser =models.ForeignKey(BaseUser, on_delete=models.CASCADE)
+    baseuser =models.OneToOneField(BaseUser, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.baseuser.name
+    
+    
+class CustomFieldData(models.Model):
+    external_type = models.CharField(max_length=64)
+    external_id = models.IntegerField()
+    field_name = models.CharField(max_length=64)
+    value = models.TextField()
+    
+    def __str__(self):
+        return '{0}-{1}-{2}'.format(self.external_type, self.external_id, self.field_name)
+
+
+class CustomFieldMetaData(models.Model):
+    external_type = models.CharField(max_length=64)
+    external_id = models.IntegerField()
+    field_name = models.CharField(max_length=64)
+    type = models.CharField(max_length=64)
+    view_type = models.CharField(max_length=64)
+    # 各种able能力
+
+    def __str__(self):
+        return '{0}-{1}-{2}'.format(self.external_type, self.external_id, self.field_name)
